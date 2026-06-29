93ff06046ee79001c024ce10f3535adf57503e86
Ken Irwin <ken@kenirwin.net>
2026-06-29 16:37:39 -0400

    add test

---
+++ test/SearchApi.test.ts
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import { describe, it, expect } from 'vitest';
+import { SearchApi } from '../dist/index.js';
+import { primoSearchResponseSchema } from '../dist/schemas/PrimoResponseSchema.js';
+
+const searchApi = new SearchApi({
+  baseUrl: process.env.BASE_URL || '',
+  apiKey: process.env.API_KEY || '',
+  vid: process.env.VID_DEFAULT || '',
+  scope: process.env.SCOPE_DEFAULT || '',
+  tab: process.env.TAB_DEFAULT || '',
+});
+
+describe('SearchApi', () => {
+  it('should find search results', async () => {
+    const searchQuery = 'any,contains,quixote';
+    const raw = await searchApi.search(searchQuery, { limit: 2, lang: 'es' });
+    const results = primoSearchResponseSchema.parse(raw);
+    expect(results).toEqual(raw);
+  });
+});
